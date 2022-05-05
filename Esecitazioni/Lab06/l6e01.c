bfb4d6e23ddc6a1c4dd17d6ed79b9297f4a39cd0
Marche22 <davidemarche22@gmail.com>
2022-05-05 11:56:57 +0200

    l6e1 e 2

---
+++ Esecitazioni/Lab06/l6e01.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void delete_new_line(char* string);
+int string_dim(char* string);
+void reverse(char* string, char* reversed);
+
+int main(void){
+
+	/* BUFSIZ e' una costante definita in stdio.h */
+	char str1[BUFSIZ], str2[BUFSIZ];
+
+	printf("Inserisci una stringa: ");
+
+	if(fgets(str1,BUFSIZ,stdin)==NULL){
+		printf("Errore in lettura!\n");
+		return -1;
+	}
+
+	delete_new_line(str1);
+
+	reverse(str1, str2);
+	//printf("\n%d", string_dim(str1));
+	printf("\nLa stringa al contrario e':\n%s\n", str2);
+}
+//Davide Marchesi
+
+void delete_new_line(char* string){
+	int i=0;
+	for (;string[i]!='\n';i++);
+	string[i]='\0';
+}
+
+int string_dim(char* string){
+	int i=0;
+	for(;(*(string+i))!='\0'; i++){}
+	return i;
+}
+
+void reverse(char* string, char* reversed){
+	int dim = string_dim(string);
+	for(int i=dim-1;i>=0;i--){
+		*(reversed+dim-1-i)=*(string+i);
+	}
+}
